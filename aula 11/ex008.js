3058e78747d6944d7c2b5210bfe6051afa1cd774
Natalia Ambrosio <natalia_ambrosio@yahoo.com.br>
2020-08-18 19:03:39 -0300

    Uso de condição em JS

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ //CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija com segurança`)
