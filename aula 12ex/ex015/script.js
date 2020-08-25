aba4df17d9a44b32de0cd9d73078a867a157bb4d
Natalia Ambrosio <natalia_ambrosio@yahoo.com.br>
2020-08-25 17:27:16 -0300

    Verificação de idade

--- aula 12ex/ex015/script.js
+++ aula 12ex/ex015/script.js
@@ -3,15 +3,18 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.` 
+       msg.innerHTML = `Agora são ${hora} horas.` 
     if(hora >=0 && hora < 12){
         //BOM DIA!!
         img.src = 'manha.jpg'
-    }else if (hora >= 12 && hora < 18){
+        document.body.style.background = '#ee9d4c'
+    }else if (hora >= 12 && hora <= 18){
        //BOA TARDE!!
        img.src = 'tarde.jpg'
+       document.body.style.background = '#72593b'
     }else {
         //BOA NOITE!!
         img.src = 'noite.jpg'
+        document.body.style.background = '#181f29'
     }
 }
