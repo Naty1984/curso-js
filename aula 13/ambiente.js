9735f1e5430a0496b965ec56216548459ed11ad5
Natalia Ambrosio <natalia_ambrosio@yahoo.com.br>
2020-09-09 18:05:02 -0300

    Repetições while/ do while

---
+++ aula 13/ambiente.js
@@ -0,0 +1,23 @@
+/*
+var c = 1 
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c ++
+}
+*/
+/*var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+*/
+/*
+var c = 1
+while (c <=6){
+    console.log (c)
+    c++
+}
+*/
+for(var c= 1;c <= 5; c++){
+    console.log(c)
+}
